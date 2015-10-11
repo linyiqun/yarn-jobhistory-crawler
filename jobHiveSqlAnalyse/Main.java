d886adc703c38c55bc02efff431ddb606f3e304a
lyq <675250079@qq.com>
2015-10-11 15:00:56 +0800

    住方法增加线程数设置参数
     住方法增加线程数设置参数

--- jobHiveSqlAnalyse/Main.java
+++ jobHiveSqlAnalyse/Main.java
@@ -6,40 +6,46 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
-	public static void main(String[] args){
-		HiveSqlAnalyseTool tool;
+	public static void main(String[] args) {
+		int threadNum;
 		String historyJobPath;
 		String dirType;
 		String dirPrefix;
-		
+
+		HiveSqlAnalyseTool tool;
+
+		threadNum = 5;
 		historyJobPath = "";
-		getPreviousDayTime();
-		if(args.length == 2){
+		if (args.length == 2 || args.length == 3) {
 			dirType = args[0];
-			
-			if(dirType.equals("dateTimeDir")){
+
+			if (dirType.equals("dateTimeDir")) {
 				dirPrefix = args[1];
 				historyJobPath = dirPrefix + getPreviousDayTime();
-			}else if (dirType.equals("desDir")){
-				historyJobPath = args[1]; 
+			} else if (dirType.equals("desDir")) {
+				historyJobPath = args[1];
 			}
-			
-		}else {
+
+			if (args.length == 3) {
+				threadNum = Integer.parseInt(args[2]);
+			}
+		} else {
 			System.out.println("input prarm is incorrect");
 			return;
 		}
-		
-		System.out.println("jobHistory dir path is " + historyJobPath);
-		tool = new HiveSqlAnalyseTool(historyJobPath);
+
+		System.out.println("dirType is ," + dirType + "jobHistory dir path is "
+				+ historyJobPath + ", parse thread num is " + threadNum);
+		tool = new HiveSqlAnalyseTool(dirType, historyJobPath, threadNum);
 		tool.readJobInfoFiles();
 	}
-	
-	private static String  getPreviousDayTime(){
+
+	private static String getPreviousDayTime() {
 		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DATE, -1); //得到前一天
+		calendar.add(Calendar.DATE, -1); // 得到前一天
 		Date date = calendar.getTime();
 		DateFormat df = new SimpleDateFormat("/yyyy/MM/dd");
-		
+
 		return df.format(date);
 	}
 }
