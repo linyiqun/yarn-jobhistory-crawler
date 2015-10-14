5c59f02642eae348df0a747b247eb31e15249a18
lyq <675250079@qq.com>
2015-10-14 19:19:15 +0800

    主方法类添加新的参数选择
    主方法类添加新的参数选择

--- jobHiveSqlAnalyse/Main.java
+++ jobHiveSqlAnalyse/Main.java
@@ -8,35 +8,48 @@ import java.util.Date;
 public class Main {
 	public static void main(String[] args) {
 		int threadNum;
+		int writedb;
 		String historyJobPath;
 		String dirType;
 		String dirPrefix;
+		String[] params;
 
 		HiveSqlAnalyseTool tool;
 
 		threadNum = 5;
+		writedb = 0;
 		historyJobPath = "";
-		if (args.length == 2 || args.length == 3) {
+		if (args.length >= 2) {
 			dirType = args[0];
 
 			if (dirType.equals("dateTimeDir")) {
 				dirPrefix = args[1];
 				historyJobPath = dirPrefix + getPreviousDayTime();
+				writedb = 1;
 			} else if (dirType.equals("desDir")) {
 				historyJobPath = args[1];
+				writedb = 0;
 			}
-
-			if (args.length == 3) {
-				threadNum = Integer.parseInt(args[2]);
+			
+			if(args.length > 2){
+			  for(int i=2; i<args.length; i++){
+			    params = args[i].split("=");
+			    
+			    if(params[0].equals("-threadnum")){
+			      threadNum = Integer.parseInt(params[1]);
+			    }else if(params[0].equals("-writedb")){
+			      writedb = Integer.parseInt(params[1]);
+			    }
+			  }
 			}
 		} else {
 			System.out.println("input prarm is incorrect");
 			return;
 		}
 
-		System.out.println("dirType is ," + dirType + "jobHistory dir path is "
-				+ historyJobPath + ", parse thread num is " + threadNum);
-		tool = new HiveSqlAnalyseTool(dirType, historyJobPath, threadNum);
+		System.out.println("dirType is " + dirType + ", jobHistory dir path is "
+				+ historyJobPath + ", parse thread num is " + threadNum + ", writedb is " + writedb);
+		tool = new HiveSqlAnalyseTool(dirType, historyJobPath, threadNum, writedb);
 		tool.readJobInfoFiles();
 	}
 
